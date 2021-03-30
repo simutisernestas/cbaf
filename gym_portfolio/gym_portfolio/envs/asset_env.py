fe50b1bd11baaa9cfca0ff9a7cf5c5cb10f1227e
Ernestas <simutisernestas@gmail.com>
2021-03-30 21:42:45 +0300

    init testing gym

--- gym_portfolio/gym_portfolio/envs/asset_env.py
+++ gym_portfolio/gym_portfolio/envs/asset_env.py
@@ -11,43 +11,59 @@ MIN_EP_LEN = 60
 MAX_EP_LEN = 60*3
 HOLD_EPSILON = 0.075
 
+
+class AssetHistoryColumns(Enum):
+    STATE = 'state_features'
+    LAST_N = 'last_n'
+
+
 class AssetEnv(gym.Env):
     metadata = {'render.modes': ['human']}
 
-    def __init__(self):
+    def __init__(self, df=None):
         super(AssetEnv, self).__init__()
+        if df is not None:
+            self._set_df(df)
+        else:
+            self.df = None
+
+        self.action_space = spaces.Box(-1, 1, (1,))
+        self.reward_range = (-np.inf, np.inf)
+
+    def _set_df(self, df):
+        for c in AssetHistoryColumns:
+            if c.value not in df.columns:
+                raise RuntimeError(
+                    f"{c} column doesn't exist in the dataframe")
 
         self.current_step = 0
-        self.df = pd.read_pickle(
-            '/home/ernestas/Desktop/stuff/py/cbaf/awesome.pkl')
+        self.df = df
         self.row_count = len(self.df.index)
-
-        his_feature_len = self.df['state_features'][self.current_step] \
+        if self.row_count < MIN_EP_LEN:
+            raise RuntimeError(f"")
+        his_feature_len = self.df[AssetHistoryColumns.STATE.value][self.current_step] \
             .reshape((-1,)).shape[0]
-        resent_steps_len = self.df['last_n'][self.current_step][:-1] \
+        resent_steps_len = self.df[AssetHistoryColumns.LAST_N.value][self.current_step][:-1] \
             .reshape((-1,)).shape[0]
-
         self.observation_space = spaces.Box(
             -np.inf, np.inf,
             shape=((his_feature_len+resent_steps_len,)),
             dtype=np.float32)
-        self.action_space = spaces.Box(-1, 1, (1,))
-        # self.action_space = spaces.Tuple( # TODO
-        #     (spaces.Discrete(3), spaces.Box(0, MAX_ACTION_SIZE, (1,))))
-        self.reward_range = (-np.inf, np.inf)
 
     def _next_observation(self):
-        steps_tail = self.df['last_n'][self.current_step][:-1].reshape(-1)
-
-        his_features = self.df['state_features'][self.current_step]
+        steps_tail = self.df[AssetHistoryColumns.LAST_N.value][self.current_step][:-1] \
+            .reshape(-1)
+        his_features = self.df[AssetHistoryColumns.STATE.value][self.current_step]
 
         return np.append(his_features, steps_tail)
 
     def _take_action(self, action):
         # Set the current price to a random price within the time step
-        current_price = random.uniform(
-            self.df["last_n"][self.current_step][-1][2],
-            self.df["last_n"][self.current_step][-1][3])
+        mean = (self.df[AssetHistoryColumns.LAST_N.value][self.current_step][-1][0] +
+                self.df[AssetHistoryColumns.LAST_N.value][self.current_step][-1][1]) / 2
+        sigma = (self.df[AssetHistoryColumns.LAST_N.value][self.current_step][-1][1] -
+                self.df[AssetHistoryColumns.LAST_N.value][self.current_step][-1][0]) / 2 / 3
+        current_price = np.random.normal(mean,sigma,(1,))[0]
 
         action = action[0]
         amount = abs(self.net_worth * MAX_ACTION_SIZE_FROM_NET * action)
@@ -63,7 +79,7 @@ class AssetEnv(gym.Env):
             amount = min(amount, self.balance)
 
             shares_bought = amount / current_price
-            
+
             self.balance -= amount
             self.shares_held += shares_bought
 
@@ -83,6 +99,9 @@ class AssetEnv(gym.Env):
             self.cost_basis = 0
 
     def step(self, action):
+        if self.df is None:
+            raise RuntimeError("No dataframe!")
+
         # Execute one time step within the environment
         self._take_action(action)
 
@@ -92,7 +111,7 @@ class AssetEnv(gym.Env):
         reward = self.net_worth - INITIAL_ACCOUNT_BALANCE
 
         if self.current_step >= (self.row_count) or self.current_step < 0 \
-            or self.ep_len > MAX_EP_LEN:
+                or self.ep_len > MAX_EP_LEN:
             self.current_step = 0
             return np.zeros((self.observation_space.shape[0],)), reward, True, {}
 
@@ -103,6 +122,9 @@ class AssetEnv(gym.Env):
         return obs, reward, done, {}
 
     def reset(self):
+        if self.df is None:
+            raise RuntimeError("No dataframe!")
+
         # Reset the state of the environment to an initial state
         self.balance = INITIAL_ACCOUNT_BALANCE
         self.net_worth = INITIAL_ACCOUNT_BALANCE
@@ -116,12 +138,15 @@ class AssetEnv(gym.Env):
         return self._next_observation()
 
     def render(self, mode='human', close=False):
+        if self.df is None:
+            raise RuntimeError("No dataframe!")
+
         # Render the environment to the screen
         profit = self.net_worth - INITIAL_ACCOUNT_BALANCE
-        print(f'Balance:\t{self.balance}')
-        print(f'Profit:\t{profit}')
-        print(f'Shared:\t{self.shares_held}')
-        print(f'Basis:\t{self.cost_basis}')
+        print(f'Balance:{self.balance}')
+        print(f'Profit:{profit}')
+        print(f'Shared:{self.shares_held}')
+        print(f'Basis:{self.cost_basis}')
 
 
 if __name__ == '__main__':
---
+++ gym_portfolio/tests/test_asset_env.py
@@ -0,0 +1,195 @@
+import unittest
+import gym
+import pandas as pd
+import numpy as np
+from gym_portfolio.envs.asset_env import AssetEnv, AssetHistoryColumns
+from gym_portfolio.envs.asset_env import INITIAL_ACCOUNT_BALANCE, MAX_ACTION_SIZE_FROM_NET
+
+# TODO: util for printing df data
+# for k in range(100):
+#     print(df.loc[k,'last_n'][-1][0], df.loc[k,'last_n'][-1][1])
+
+
+class TestStringMethods(unittest.TestCase):
+
+    # TODO: ???
+    # def setUp(self):
+    #     self.env.reset()
+
+    @classmethod
+    def setUpClass(cls):
+        "Hook method for setting up class fixture before running tests in the class."
+        cls.env = gym.make('gym_portfolio:asset-v0')
+
+    def test_env_doesnt_step_without_dataframe(self):
+        self.env = gym.make('gym_portfolio:asset-v0')
+        with self.assertRaises(RuntimeError):
+            self.env.step(self.env.action_space.sample())
+
+    def test_env_doesnt_reset_without_dataframe(self):
+        self.env = gym.make('gym_portfolio:asset-v0')
+        with self.assertRaises(RuntimeError):
+            self.env.reset()
+
+    def test_env_doesnt_render_without_dataframe(self):
+        self.env = gym.make('gym_portfolio:asset-v0')
+        with self.assertRaises(RuntimeError):
+            self.env.render()
+
+    def test_step_without_reset_throws_error(self):
+        # TODO:
+        self.assertTrue(True)
+
+    def make_empty_asset_history_df(self):
+        d = {}
+        for c in AssetHistoryColumns:
+            d[c.value] = []
+        return pd.DataFrame(data=d)
+
+    def make_increasing_sequence(self):
+        df = self.make_empty_asset_history_df()
+        last_n = np.zeros((11, 5))
+        # [ low, high, open, close, volume ]
+        for i, step in enumerate(last_n):
+            step[0] = i * 1.2
+            step[1] = i * 1.3
+            step[2] = i * 1.2
+            step[3] = i * 1.3
+            step[4] = 0.5
+        for j in range(100):
+            last_n[-1][0] = last_n[-1][0]*1.12
+            last_n[-1][1] = last_n[-1][1]*1.12
+            last_n[-1][2] = last_n[-1][2]*1.12
+            last_n[-1][3] = last_n[-1][3]*1.12
+            df.loc[j] = [np.ones((50,)), np.copy(last_n)]
+        return df
+
+    def test_appreciating_asset_continous_buy_results_in_profit(self):
+        df = self.make_increasing_sequence()
+        self.env._set_df(df)
+        self.env.reset()
+        start_index = self.env.current_step
+        self.assertEqual(self.env.balance, INITIAL_ACCOUNT_BALANCE)
+        while self.env.balance > 0:
+            _, _, done, _ = self.env.step([1])
+            if done:
+                break
+        self.assertTrue(self.env.balance < INITIAL_ACCOUNT_BALANCE)
+        self.assertTrue(self.env.net_worth > INITIAL_ACCOUNT_BALANCE)
+
+        end_index = self.env.current_step
+
+        start_low = self.env.df.loc[start_index, 'last_n'][-1][0]
+        end_low = self.env.df.loc[end_index, 'last_n'][-1][0]
+
+        ration = end_low/start_low
+        bid = INITIAL_ACCOUNT_BALANCE * MAX_ACTION_SIZE_FROM_NET
+        growth_on_const_buy_amount = ration * bid
+
+        self.assertTrue(growth_on_const_buy_amount < (
+            self.env.net_worth - INITIAL_ACCOUNT_BALANCE))
+
+    def test_appreciating_asset_buy_profit_is_correct(self):
+        df = self.make_increasing_sequence()
+        self.env._set_df(df)
+        self.env.reset()
+        self.assertEqual(self.env.balance, INITIAL_ACCOUNT_BALANCE)
+        self.env.step([1])
+        self.assertTrue(self.env.net_worth == INITIAL_ACCOUNT_BALANCE,
+                        f"{self.env.net_worth} == {INITIAL_ACCOUNT_BALANCE}")
+        self.env.step([1])
+        self.assertTrue(self.env.net_worth > INITIAL_ACCOUNT_BALANCE,
+                        f"{self.env.net_worth} > {INITIAL_ACCOUNT_BALANCE}")
+
+        end_index = self.env.current_step - 1
+
+        current_price_bottom = self.env.df.loc[end_index, 'last_n'][-1][0]
+        current_price_top = self.env.df.loc[end_index, 'last_n'][-1][1]
+
+        range_bottom = self.env.shares_held * current_price_bottom + self.env.balance
+        range_top = self.env.shares_held * current_price_top + self.env.balance
+
+        self.assertTrue(self.env.net_worth > range_bottom,
+                        f"{self.env.net_worth} > {range_bottom}")
+        self.assertTrue(self.env.net_worth < range_top,
+                        f"{self.env.net_worth} < {range_top}")
+
+    def make_decreasing_sequence(self):
+        df = self.make_empty_asset_history_df()
+        last_n = np.zeros((11, 5))
+        # [ low, high, open, close, volume ]
+        for i, step in enumerate(last_n):
+            step[0] = i * 0.7
+            step[1] = i * 0.9
+            step[2] = i * 0.7
+            step[3] = i * 0.9
+            step[4] = 0.5
+        for j in range(100):
+            last_n[-1][0] = last_n[-1][0]*0.7
+            last_n[-1][1] = last_n[-1][1]*0.7
+            last_n[-1][2] = last_n[-1][2]*0.7
+            last_n[-1][3] = last_n[-1][3]*0.7
+            df.loc[j] = [np.ones((50,)), np.copy(last_n)]
+        return df
+
+    def test_depreciating_asset_continous_buy_results_in_loss(self):
+        df = self.make_decreasing_sequence()
+        self.env._set_df(df)
+        self.env.reset()
+        start_index = self.env.current_step
+        self.assertEqual(self.env.balance, INITIAL_ACCOUNT_BALANCE)
+        while self.env.balance > 0:
+            _, _, done, _ = self.env.step([1])
+            if done:
+                break
+        self.assertTrue(self.env.balance < INITIAL_ACCOUNT_BALANCE)
+        self.assertTrue(self.env.net_worth < INITIAL_ACCOUNT_BALANCE)
+
+        end_index = self.env.current_step
+
+        start_low = self.env.df.loc[start_index, 'last_n'][-1][0]
+        end_low = self.env.df.loc[end_index, 'last_n'][-1][0]
+
+        ration = end_low/start_low
+        bid = INITIAL_ACCOUNT_BALANCE * MAX_ACTION_SIZE_FROM_NET
+        loss_on_const_buy_amount = ration * bid
+
+        self.assertTrue(loss_on_const_buy_amount > (
+            self.env.net_worth - INITIAL_ACCOUNT_BALANCE))
+
+    def test_depreciating_asset_buy_loss_is_correct(self):
+        df = self.make_decreasing_sequence()
+        self.env._set_df(df)
+        self.env.reset()
+        self.assertEqual(self.env.balance, INITIAL_ACCOUNT_BALANCE)
+        self.env.step([1])
+        self.assertTrue(self.env.net_worth == INITIAL_ACCOUNT_BALANCE,
+                        f"{self.env.net_worth} == {INITIAL_ACCOUNT_BALANCE}")
+        self.env.step([1])
+        self.assertTrue(self.env.net_worth < INITIAL_ACCOUNT_BALANCE,
+                        f"{self.env.net_worth} < {INITIAL_ACCOUNT_BALANCE}")
+
+        end_index = self.env.current_step - 1
+
+        current_price_bottom = self.env.df.loc[end_index, 'last_n'][-1][0]
+        current_price_top = self.env.df.loc[end_index, 'last_n'][-1][1]
+
+        range_bottom = self.env.shares_held * current_price_bottom + self.env.balance
+        range_top = self.env.shares_held * current_price_top + self.env.balance
+
+        self.assertTrue(self.env.net_worth > range_bottom,
+                        f"{self.env.net_worth} > {range_bottom}")
+        self.assertTrue(self.env.net_worth < range_top,
+                        f"{self.env.net_worth} < {range_top}")
+
+    # TODO: test_sell_appreciating
+    # TODO: test_sell_depreciating
+    # TODO: test_buy_sell_appreciating
+    # TODO: test_buy_sell_depreciating
+    # TODO: test_sell_buy_appreciating
+    # TODO: test_sell_buy_depreciating
+    # TODO: test_sell_buy_sell_maybe
+    # TODO: test_buy_sell_buy_maybe
+
+if __name__ == '__main__':
+    unittest.main()
